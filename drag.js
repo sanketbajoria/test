23e00cab34b21f44ef051f33fb6c3282bfa85feb
sanketbajoria <bajoriasanket@gmail.com>
2013-08-01 12:11:56 +0530

    Update drag.js

--- drag.js
+++ drag.js
@@ -1,22 +1,21 @@
 angular.module('drag', []).
 directive('draggable', function($document) {
   return function(scope, element, attr) {
-    var startX = 0, startY = 0, startTop=toNum(element.css('top')), startLeft=toNum(element.css('left')), x = 0, y = 0;
+    var startX = 0, startY = 0, 
+        startTop=toNum(element.css('top')),                 startLeft=toNum(element.css('left')), x = 0, y = 0;
     var shadow = null;
+    var isRelative = (element.css('position') == 'relative')?true:false;
     element.css({
-     border: '1px solid red',
-     backgroundColor: 'lightgrey',
-     cursor: 'pointer'
+      cursor: 'pointer'
     });
-      var isRelative = (element.css('position') == 'relative')?true:false;
+      
     element.bind('mousedown', function(event) {
       // Prevent default dragging of selected content
       event.preventDefault();
-      startX = event.screenX;
-      startY = event.screenY;
-        startTop = isRelative?startTop:element[0].offsetTop;
+      startX = event.pageX;
+      startY = event.pageY;
+      startTop = isRelative?startTop:element[0].offsetTop;
       startLeft = isRelative?startLeft:element[0].offsetLeft;
-        
       $document.bind('mousemove', mousemove);
       $document.bind('mouseup', mouseup);
     });
@@ -25,36 +24,33 @@ directive('draggable', function($document) {
           return angular.element("<div></div>").css({width:element[0].offsetWidth+'px',height:element[0].offsetHeight+'px',backgroundColor: 'lightBlue', border: '1px solid darkCyan', position: 'absolute'}).addClass("shadow");
       }
     function mousemove(event) {
-      axis = attr.axis;  
-        y = (!axis || axis == 'y')?(event.screenY - startY):element.prop('top');
-        x = (!axis || axis == 'x')?(event.screenX - startX):element.prop('left');
+        y = event.pageY - startY;
+        x = event.pageX - startX;
         if(attr.helper){
         if(shadow == null){
             shadow = (attr.helper == 'clone')?getShadow():attr.helper();
             element.after(shadow);
         }
-                 
-      shadow.css({
-        top: y + element[0].offsetTop + 'px',
-        left:  x + element[0].offsetLeft + 'px',
-          display: 'block'
-      });
+            setPos(shadow,isAxis('y')?y + element[0].offsetTop:element[0].offsetTop, isAxis('x')?x + element[0].offsetLeft:element[0].offsetLeft).css({display: 'block'})         
         } else {
-        element.css({top: y + startTop +'px',left: x  + startLeft + 'px'})
-        }
+        setPos(element,isAxis('y') && y + startTop,isAxis('x') && x  + startLeft);        
+       }
     }
       function toNum(x){
           return isNaN(parseInt(x))?0:parseInt(x);
       }
     function mouseup() {
-      element.css({top: y + startTop +'px',left: x  + startLeft + 'px'})
-      startTop += y;
-      startLeft += x;
+      setPos(element,isAxis('y') && y + startTop,isAxis('x') && x  + startLeft);
+        startTop += isAxis('y')?y:0;
+        startLeft += isAxis('x')?x:0;
       if(attr.helper)     
           shadow.css({display:'none'});
       $document.unbind('mousemove', mousemove);
       $document.unbind('mouseup', mouseup);
     }
+      function isAxis(k){
+          return !attr.axis || attr.axis == k;
+      }
       function setPos(obj, top, left){
           var s = {};
           if(top)
@@ -62,6 +58,7 @@ directive('draggable', function($document) {
           if(left)
               s["left"] = left + 'px';
           obj.css(s);
+          return obj;
       }
   }
 });
